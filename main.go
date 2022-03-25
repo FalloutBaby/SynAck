af26072ee44a945d68b6b32c8ab23d62a44dd14a
Stanislava <ferrashe4ka@mail.ru>
2022-03-25 20:48:30 +0300

    AP-1, feat: scanning and printing
    Co-Authored-By: danabolik <30245017+danabolik@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package SynAck
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var input int
+	fmt.Print("Выберите количество потоков: ")
+	_, err := fmt.Scanf("%d", &input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Збазиба!")
+}
