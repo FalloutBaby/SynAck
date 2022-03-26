a968f69c7fab3116e58021e0437e5137796f0d5f
danabolik <kvaciy2905@gmail.com>
2022-03-26 12:48:10 +0300

    AP-1, test: Добавлен тест на продюсера

--- producer/Producer_test.go
+++ producer/Producer_test.go
@@ -1,1 +1,14 @@
 package producer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetPorts(t *testing.T) {
+	ports := []string{
+		"80", "280", "443", "488", "591", "593", "623", "664", "777", "832",
+		"1128", "1129", "1183", "1184", "5000", "5001", "8008", "8080", "11371",
+	}
+	assert.Equal(t, ports, GetPorts())
+}
