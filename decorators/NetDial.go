c68d368ee8430db33066562191eab87a7fe66db4
Stanislava <ferrashe4ka@mail.ru>
2022-03-27 12:18:56 +0300

    AP-1, feat: error handling
    Co-Authored-By: danabolik <30245017+danabolik@users.noreply.github.com>

--- decorators/NetDial.go
+++ decorators/NetDial.go
@@ -1,13 +1,21 @@
 package decorators
 
-import "net"
+import (
+	"log"
+	"net"
+)
 
 func Dial(network, addr string, ps []string) []string {
 	var result []string
 	for _, p := range ps {
-		_, err := net.Dial(network, addr+":"+p)
+		c, err := net.Dial(network, addr+":"+p)
 		if err != nil {
+			continue
 		} else {
+			err := c.Close()
+			if err != nil {
+				log.Panic(err)
+			}
 			result = append(result, p)
 		}
 	}
--- decorators/NetDial_test.go
+++ decorators/NetDial_test.go
@@ -8,13 +8,10 @@ import (
 func TestDial(t *testing.T) {
 	network := "tcp"
 	address := "scanme.nmap.org"
+	ports := []string{"80", "280", "443", "488", "591", "593", "623",
+		"664", "777", "832", "1128", "1129", "1183", "1184",
+		"5000", "5001", "8008", "8080", "11371"}
 
-	ps := Dial(
-		network,
-		address,
-		[]string{"80", "280", "443", "488", "591", "593", "623",
-			"664", "777", "832", "1128", "1129", "1183", "1184",
-			"5000", "5001", "8008", "8080", "11371"},
-	)
+	ps := Dial(network, address, ports)
 	assert.Equal(t, ps, []string{"80"})
 }
