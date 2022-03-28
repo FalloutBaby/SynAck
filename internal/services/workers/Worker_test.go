5dbadf1b5faaf58453cbb2ece352518c26b5a172
danabolik <kvaciy2905@gmail.com>
2022-03-28 18:05:20 +0300

    AP-1, test: Воркер протестирован

--- internal/services/workers/Worker_test.go
+++ internal/services/workers/Worker_test.go
@@ -7,37 +7,45 @@ import (
 )
 
 type DialerStub struct {
-	openPs    []string
-	callCount int
+	network string
+	addr    string
+	openPs  []string
+	cntGs   int
 }
 
 func (d *DialerStub) DialAll(network, addr string, ps []string) string {
-	d.callCount += 1
+	d.network = network
+	d.addr = addr
+	d.cntGs += 1
+
 	currentOpenPs := strings.Join(ps, ", ")
 	d.openPs = append(d.openPs, currentOpenPs)
 	return currentOpenPs
 }
 
 type dataProvider struct {
-	network     string
-	addr        string
-	grt         int
-	ports       []string
-	resultPorts []string
+	network string
+	addr    string
+	grt     int
+	ps      []string
 }
 
 func TestScan(t *testing.T) {
 	tests := dataProvider{
-		addr: "scanme.nmap.org",
-		grt:  5,
-		ports: []string{"80", "280", "443", "488", "591", "593", "623",
+		network: "tcp",
+		addr:    "scanme.nmap.org",
+		grt:     5,
+		ps: []string{"80", "280", "443", "488", "591", "593", "623",
 			"664", "777", "832", "1128", "1129", "1183", "1184",
 			"5000", "5001", "8008", "8080", "11371"},
-		resultPorts: []string{"80"},
 	}
+
 	dialer := DialerStub{}
 	w := Worker{Decorator: &dialer}
-	result := w.Scan(tests.addr, tests.ports, tests.grt)
+	result := w.Scan(tests.addr, tests.ps, tests.grt)
+
+	assert.Equal(t, dialer.network, tests.network)
+	assert.Equal(t, dialer.addr, tests.addr)
+	assert.Equal(t, dialer.cntGs, tests.grt)
 	assert.Equal(t, dialer.openPs, result)
-	assert.Equal(t, dialer.callCount, tests.grt)
 }
