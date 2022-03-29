4fef80b372f645eb079d6a92af4290b828d0a69d
danabolik <kvaciy2905@gmail.com>
2022-03-29 20:07:04 +0300

    AP-1, feat: Воркер в порядке

--- internal/services/workers/Worker.go
+++ internal/services/workers/Worker.go
@@ -15,58 +15,24 @@ type Worker struct {
 
 func (w Worker) ScanPorts(addr string, grt int) []int {
 	tcp := w.Delivery.GetNetwork()
-	amount := 81
-	psChan := make(chan int, amount)
-
 	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		w.Producer.WritePsToChan(&psChan)
-	}()
 
-	chanel := make(chan int, cap(psChan)-1)
+	psChan := make(chan int, w.Producer.GetCountPorts())
+	go w.Producer.WritePsToChan(psChan)
+
+	var result []int
 	for i := 0; i < grt; i++ {
 		wg.Add(1)
-
 		go func() {
 			defer wg.Done()
-			var cycle int
-			var exit bool
-			for {
-				p, open := <-psChan
-				if len(psChan) != 0 && open {
-					dial := w.Decorator.DialPort(tcp, addr, p)
-					chanel <- dial
-				} else if len(psChan) == 0 && open {
-					close(psChan)
-					exit = true
-					break
-				} else {
-					exit = true
-					break
+			for p := range psChan {
+				dial := w.Decorator.DialPort(tcp, addr, p)
+				if dial != 0 {
+					result = append(result, dial)
 				}
-				cycle++
-			}
-
-			if exit {
-				return
 			}
 		}()
 	}
-
-	var result []int
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < cap(chanel); i++ {
-			ps := <-chanel
-			if ps != 0 {
-				result = append(result, ps)
-			}
-		}
-		close(chanel)
-	}()
 	wg.Wait()
 
 	return result
